fb5ccc7a2fac2b96e10a95a540454909b810c054
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-08-30 02:10:34 +0800

    chore: 脚本配置

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     monkey({
       entry: 'src/main.ts',
       userscript: {
-        icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48ZyBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIxLjUiPjxwYXRoIGQ9Ik02IDExYzAtMi44MjggMC00LjI0My44NzktNS4xMjFDNy43NTcgNSA5LjE3MiA1IDEyIDVoM2MyLjgyOCAwIDQuMjQzIDAgNS4xMjEuODc5QzIxIDYuNzU3IDIxIDguMTcyIDIxIDExdjVjMCAyLjgyOCAwIDQuMjQzLS44NzkgNS4xMjFDMTkuMjQzIDIyIDE3LjgyOCAyMiAxNSAyMmgtM2MtMi44MjggMC00LjI0MyAwLTUuMTIxLS44NzlDNiAyMC4yNDMgNiAxOC44MjggNiAxNnoiLz48cGF0aCBkPSJNNiAxOWEzIDMgMCAwIDEtMy0zdi02YzAtMy43NzEgMC01LjY1NyAxLjE3Mi02LjgyOEM1LjM0MyAyIDcuMjI5IDIgMTEgMmg0YTMgMyAwIDAgMSAzIDMiLz48L2c+PC9zdmc+',
+        icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDMyIDMyIj48cGF0aCBmaWxsPSJjdXJyZW50Q29sb3IiIGQ9Im0yMyA0bC01IDMuNzV2Ni41TDE1IDEybC01IDMuNzV2Ni41TDcgMjBsLTUgMy43NVYzMGgydi01LjI1bDMtMi4yNWwzIDIuMjVWMzBoMlYxNi43NWwzLTIuMjVsMyAyLjI1VjMwaDJWOC43NWwzLTIuMjVsMyAyLjI1VjMwaDJWNy43NXoiLz48L3N2Zz4=',
         namespace: 'https://blog.adproqwq.xyz',
         match: ['https://i.gkd.li/*'],
         exclude: ['https://i.gkd.li'],
@@ -23,6 +23,8 @@ export default defineConfig({
         grant: 'none',
         description: '让GKD网页审查工具再次强大！',
         license: 'MIT',
+        downloadURL: encodeURI('https://update.greasyfork.org/scripts/488829/GKD网页审查工具增强.user.js'),
+        updateURL: encodeURI('https://update.greasyfork.org/scripts/488829/GKD网页审查工具增强.meta.js'),
       },
       build: {
         externalGlobals: {
