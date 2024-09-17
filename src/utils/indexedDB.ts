17412246de0243fa6a7e1775e0faae194d7488db
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-09-17 15:01:30 +0800

    perf: 优化editNode函数传参格式

--- src/utils/indexedDB.ts
+++ src/utils/indexedDB.ts
@@ -23,14 +23,14 @@ export const simplyActivityIds = async (snapshotId: string): Promise<string | fa
   else return false;
 };
 
-export const editNode = async (snapshotId: string, nodeId: number, option: EditNodeOption): Promise<boolean> => {
+export const editNode = async (snapshotId: string, nodeId: number, options: EditNodeOption[]): Promise<boolean> => {
   try{
     const snapshotInfo = await snapshotStorage.getItem<Snapshot>(snapshotId);
 
     const nodes = snapshotInfo!.nodes;
     const nodeAttr = nodes[nodeId].attr;
 
-    (nodeAttr[option.target] as PrimitiveType) = option.value;
+    options.forEach((option) => (nodeAttr[option.target] as PrimitiveType) = option.value);
 
     nodes[nodeId].attr = nodeAttr;
     snapshotInfo!.nodes = nodes;
