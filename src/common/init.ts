a7639e408b06719e672e1e19560baa2d8fa78f58
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-09-21 15:33:40 +0800

    chore: bump version

--- src/common/init.ts
+++ src/common/init.ts
@@ -3,7 +3,7 @@ import { snackbar } from 'mdui';
 if(!window.localStorage.getItem('selectors')) window.localStorage.setItem('selectors', '[]');
 
 snackbar({
-  message: '低声问向谁行宿，城上已三更。',
+  message: '滩声秋更急，峡气晓多阴。',
   autoCloseDelay: 3000,
   placement: 'top',
 });
