624e66c7054a3226bfcc7592982287fc6d24056f
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-09-21 21:52:51 +0800

    chore: bump version

--- src/common/init.ts
+++ src/common/init.ts
@@ -3,7 +3,7 @@ import { snackbar } from 'mdui';
 if(!window.localStorage.getItem('selectors')) window.localStorage.setItem('selectors', '[]');
 
 snackbar({
-  message: '滩声秋更急，峡气晓多阴。',
+  message: '对秋深，离恨苦，数夜满庭风雨。',
   autoCloseDelay: 3000,
   placement: 'top',
 });
