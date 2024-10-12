31eb6e3ff445db339a80241b0cf4d5c8b0fd12b6
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-10-12 20:01:21 +0800

    fix: 隐藏搜索框后重新打开按钮没有被重新插入

--- src/common/insertIcon.ts
+++ src/common/insertIcon.ts
@@ -3,57 +3,60 @@ import observeElement from '../utils/observeElement';
 import replaceNodeInfo from '../utils/replaceNodeInfo';
 
 observeElement('.n-input-group', () => {
-  const inputGroup = document.querySelector('.n-input-group')!;
-  const iconBar = document.createElement('div');
+  if(!document.querySelector('#iconBar')){
+    const inputGroup = document.querySelector('.n-input-group')!;
+    const iconBar = document.createElement('div');
+    iconBar.id = 'iconBar';
 
-  // 搜索选择器按钮
-  const UseSelectorIcon = document.createElement('mdui-button-icon');
-  UseSelectorIcon.icon = 'search';
-  UseSelectorIcon.style.height = '36px';
-  UseSelectorIcon.style.width = '36px';
-  UseSelectorIcon.onclick = () => {
-    send('openUseSelector');
-  };
+    // 搜索选择器按钮
+    const UseSelectorIcon = document.createElement('mdui-button-icon');
+    UseSelectorIcon.icon = 'search';
+    UseSelectorIcon.style.height = '36px';
+    UseSelectorIcon.style.width = '36px';
+    UseSelectorIcon.onclick = () => {
+      send('openUseSelector');
+    };
 
-  // 添加选择器按钮
-  const AddSelectorIcon = document.createElement('mdui-button-icon');
-  AddSelectorIcon.icon = 'add';
-  AddSelectorIcon.style.height = '36px';
-  AddSelectorIcon.style.width = '36px';
-  AddSelectorIcon.onclick = () => {
-    send('openAddSelector');
-  };
+    // 添加选择器按钮
+    const AddSelectorIcon = document.createElement('mdui-button-icon');
+    AddSelectorIcon.icon = 'add';
+    AddSelectorIcon.style.height = '36px';
+    AddSelectorIcon.style.width = '36px';
+    AddSelectorIcon.onclick = () => {
+      send('openAddSelector');
+    };
 
-  // 管理选择器按钮
-  const ManageSelectorsIcon = document.createElement('mdui-button-icon');
-  ManageSelectorsIcon.icon = 'edit';
-  ManageSelectorsIcon.style.height = '36px';
-  ManageSelectorsIcon.style.width = '36px';
-  ManageSelectorsIcon.onclick = () => {
-    send('openManageSelectors');
-  };
+    // 管理选择器按钮
+    const ManageSelectorsIcon = document.createElement('mdui-button-icon');
+    ManageSelectorsIcon.icon = 'edit';
+    ManageSelectorsIcon.style.height = '36px';
+    ManageSelectorsIcon.style.width = '36px';
+    ManageSelectorsIcon.onclick = () => {
+      send('openManageSelectors');
+    };
 
-  // 设置按钮
-  const SettingsIcon = document.createElement('mdui-button-icon');
-  SettingsIcon.icon = 'settings';
-  SettingsIcon.style.height = '36px';
-  SettingsIcon.style.width = '36px';
-  SettingsIcon.onclick = () => {
-    send('openSettings');
-  };
+    // 设置按钮
+    const SettingsIcon = document.createElement('mdui-button-icon');
+    SettingsIcon.icon = 'settings';
+    SettingsIcon.style.height = '36px';
+    SettingsIcon.style.width = '36px';
+    SettingsIcon.onclick = () => {
+      send('openSettings');
+    };
 
-  // 帮助按钮
-  const HelpIcon = document.createElement('mdui-button-icon');
-  HelpIcon.icon = 'help';
-  HelpIcon.style.height = '36px';
-  HelpIcon.style.width = '36px';
-  HelpIcon.onclick = () => {
-    send('openHelp');
-  };
+    // 帮助按钮
+    const HelpIcon = document.createElement('mdui-button-icon');
+    HelpIcon.icon = 'help';
+    HelpIcon.style.height = '36px';
+    HelpIcon.style.width = '36px';
+    HelpIcon.onclick = () => {
+      send('openHelp');
+    };
 
-  iconBar.append(UseSelectorIcon, AddSelectorIcon, ManageSelectorsIcon, SettingsIcon, HelpIcon);
-  inputGroup.insertAdjacentElement('beforebegin', iconBar);
-});
+    iconBar.append(UseSelectorIcon, AddSelectorIcon, ManageSelectorsIcon, SettingsIcon, HelpIcon);
+    inputGroup.insertAdjacentElement('beforebegin', iconBar);
+  }
+}, true);
 
 observeElement('.z-1.box-shadow-dim.bg-white', () => {
   const app = document.querySelector('#app')!;
