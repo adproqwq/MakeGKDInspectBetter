a87863e8780f472abaa2f0126f236f1851b3af7c
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-10-26 20:24:53 +0800

    perf: 更严谨的rulesKey排序检测

--- src/common/init.ts
+++ src/common/init.ts
@@ -19,18 +19,22 @@ const rulesKeySort = [
 ];
 
 if(!(await getHanashiroSettings('selectors'))) await setHanashiroSettings('selectors', []);
+
 if(!(await getHanashiroSettings('rulesKeySort')) || (await getHanashiroSettings<Array<string>>('rulesKeySort'))!.length == 0){
   await setHanashiroSettings('rulesKeySort', rulesKeySort);
 }
-if((await getHanashiroSettings<Array<string>>('rulesKeySort'))!.length != rulesKeySort.length) confirm({
-  headline: '同步最新rulesKey排序',
-  description: '检测你的rulesKey排序不是最新的，可能无法使用最新的功能。是否同步？注意：这会丢失你现有的排序设置。',
-  closeOnEsc: true,
-  closeOnOverlayClick: true,
-  confirmText: '同步',
-  cancelText: '取消',
-  onConfirm: async () => await setHanashiroSettings('rulesKeySort', rulesKeySort),
-});
+
+for(const rulesKey of (await getHanashiroSettings<Array<string>>('rulesKeySort'))!){
+  if(!rulesKeySort.includes(rulesKey)) confirm({
+    headline: '同步最新rulesKey排序',
+    description: '检测你的rulesKey排序有缺失的字段，可能无法使用最新的功能。是否同步？注意：这会丢失你现有的排序设置。',
+    closeOnEsc: true,
+    closeOnOverlayClick: true,
+    confirmText: '同步',
+    cancelText: '取消',
+    onConfirm: async () => await setHanashiroSettings('rulesKeySort', rulesKeySort),
+  });
+}
 
 Object.defineProperty(window, 'Hanashiro', {
   value: {},
