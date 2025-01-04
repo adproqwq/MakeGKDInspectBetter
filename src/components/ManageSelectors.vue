bcba5d9ce459e5758fe1f072282f01102db73537
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-01-04 20:36:51 +0800

    perf: 取消选择器排序优先值最小值限制

--- src/components/ManageSelectors.vue
+++ src/components/ManageSelectors.vue
@@ -62,7 +62,7 @@ export default defineComponent({
     <div>
       <span>排序优先值：</span>
       <mdui-text-field variant="filled" id="order" label="排序优先值" type="number" @change="editSelector"></mdui-text-field>
-      <span class="introduction">数字越大，排序越前，最小为1。失焦保存</span>
+      <span class="introduction">数字越大，排序越前。失焦保存</span>
     </div>
     <div>
       <mdui-button slot="action" variant="tonal" @click="close">关闭</mdui-button>
