54aae72f9fb80402d7b5bae4d8a91e65aeb722f1
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-02-05 15:08:38 +0800

    fix: 调整自动动作执行时机

--- src/AutoAction/act.ts
+++ src/AutoAction/act.ts
@@ -2,18 +2,24 @@ import { encodeURI } from 'js-base64';
 import getSnapshotId from '../utils/getSnapshotId';
 import { getAutoAction } from '../utils/indexedDB';
 
-const autoAction = await getAutoAction(getSnapshotId());
+const intervalId = setInterval(async () => {
+  if(window.location.href.includes('sanpshot')){
+    const autoAction = await getAutoAction(getSnapshotId());
 
-if (autoAction.autoSearchSelector) {
-  const url = new URL(window.location.href);
+    if (autoAction.autoSearchSelector) {
+      const url = new URL(window.location.href);
 
-  if (!url.searchParams.get('gkd')) {
-    const selectorBase64 = encodeURI(
-      autoAction.autoSearchSelector.replaceAll('\"', '"'),
-    );
+      if (!url.searchParams.get('gkd')) {
+        const selectorBase64 = encodeURI(
+          autoAction.autoSearchSelector.replaceAll('\"', '"'),
+        );
 
-    url.searchParams.set('gkd', selectorBase64);
+        url.searchParams.set('gkd', selectorBase64);
 
-    window.location.href = url.toString();
+        window.location.href = url.toString();
+      }
+    }
+
+    clearInterval(intervalId);
   }
-}
+}, 100);
